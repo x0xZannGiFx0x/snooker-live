9fc02988cb02e301fafd5bb190bbb7c7c1a84fd9
x0xZannGiFx0x <x0xZannGiFx0x@users.noreply.github.com>
2026-03-01 00:53:59 +0000

    Fixed Express 5 catchall route syntax

--- snooker-server/server.js
+++ snooker-server/server.js
@@ -84,7 +84,7 @@ const PORT = process.env.PORT || 3001;
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../snooker-client/dist', 'index.html'));
 });
 
